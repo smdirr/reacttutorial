aac23c37e848340e6c48172ca3b9b9bb0ae3e410
smdirr <smdirr@gmail.com>
2019-05-05 23:37:17 -0300

    add ul li

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0 };
+  state = { count: 0, tags: ["tag1", "tag2", "tag3"] };
 
   render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
-    return (
-      <React.Fragment>
-        <span className={classes}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
-    );
+    return classes;
   }
 
   formatCount() {
